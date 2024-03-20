1e4a50f2a0e7c3eb8fc68ca5f2708c4453a2e8ca
geonmo.nine <geonmo.nine@navercorp.com>
2024-03-20 18:50:10 +0900

    model info 초기세팅

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare const DeviceInfo: {
+  ios: Record<string, string>;
+  aos: Record<string, string>;
+};
+export default DeviceInfo;
---
+++ index.js
@@ -0,0 +1,7 @@
+import ios from "./ios.json";
+import aos from "./ios.json";
+
+export default {
+  ios,
+  aos,
+};
