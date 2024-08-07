ad00b27c82dc67559d5756a72416c2ae4188679f
yceffort <yc.effort@navercorp.com>
2024-08-07 10:54:34 +0900

    [#47] 타입단언 유틸 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 export { default as ios } from "./data/ios.json";
 export { default as aos } from "./data/aos.json";
 export { default as date } from "./data/date.js";
+
+import ios from "./data/ios.json";
+import aos from "./data/aos.json";
+
+export function isIOSDeviceString(value: unknown): value is keyof typeof ios {
+  return (
+    typeof value === "string" && ios[value as keyof typeof ios] !== undefined
+  );
+}
+
+export function isAndroidDeviceString(
+  value: unknown
+): value is keyof typeof aos {
+  return (
+    typeof value === "string" && aos[value as keyof typeof aos] !== undefined
+  );
+}
