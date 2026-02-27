07d6d0fa3ef47510b97f5ed6ef59743c3b7978df
yceffort <yceffort@gmail.com>
2026-02-27 17:42:42 +0900

    ♻️ Replace date.js with date.json

--- src/data/date.js
+++
@@ -1,3 +0,0 @@
-/*This file was automatically generated. Do not modify it manually.*/
-const date = "2026-02-27T08:10:10.759Z"
-export default date;
--- src/date.mjs
+++ src/date.mjs
@@ -3,10 +3,8 @@ import fs from "fs/promises";
 async function writeDate() {
   const now = new Date();
   await fs.writeFile(
-    "./src/data/date.js",
-    `/*This file was automatically generated. Do not modify it manually.*/
-const date = "${now.toISOString()}"
-export default date;`
+    "./src/data/date.json",
+    JSON.stringify(now.toISOString())
   );
 }
 
